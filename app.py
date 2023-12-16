678b7a063ab4269bf0604ef68a2146388b950815
aldi y a <berantemyok@gmail.com>
2023-12-16 23:03:33 +0700

    update restapi

--- app.py
+++ app.py
@@ -38,12 +38,19 @@ DELETE FROM users WHERE id = %s;
 
 SELECT_ALL_USERS = "SELECT * FROM users"
 
+UPDATE_USER_BY_ID = """
+UPDATE users
+SET username = %s, password = %s
+WHERE id = %s;
+"""
+
 load_dotenv()
 
 app = Flask(__name__)
 url = os.getenv("DATABASE_URL")
 connection = psycopg2.connect(url);
 
+# create
 @app.post("/api/users")
 def create_users():
     data = request.get_json()
@@ -57,6 +64,7 @@ def create_users():
     return {"id" : user_id, "message" : f"users {username} berhasil dibuat"}, 201
     
 
+# menampilkan satu user
 @app.get("/api/users/<int:user_id>")
 def get_user(user_id):
     with connection:
@@ -67,6 +75,7 @@ def get_user(user_id):
                 return {"message" : f"user dengan id {user_id} tidak ditemukan"}, 404
     return {"id" : user_id, "username" : user[1], "password" : user[2]}, 200
 
+# meghapus user
 @app.route('/api/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
     with connection:
@@ -80,6 +89,7 @@ def delete_user(user_id):
             connection.commit()
     return {"message" : f"user dengan id {user_id} berhasil dihapus"}, 200
 
+# mengambil seluruh user
 @app.get("/api/users/")
 def get_users():
     with connection:
@@ -90,3 +100,22 @@ def get_users():
                 return {"message" : "tidak ada user yang tersedia"}, 404
 
     return {"message" : "daftar user yang tersedia", "users" :  users}, 200
+
+
+# update
+@app.route('/api/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    data = request.get_json()
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_USER_BY_ID, (user_id,))
+            user = cursor.fetchone()
+            if user is None:
+                return {"message" : f"user dengan id {user_id} tidak ditemukan"}, 404
+
+            username = data['username']
+            password = data['password']
+
+            cursor.execute(UPDATE_USER_BY_ID, (username, password, user_id))
+            connection.commit()
+    return {"message" : f"user dengan id {user_id} berhasil diupdate"}, 200
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Berantemyok\Desktop\UTS\SEMESTER 3\pl sql\restapi_yt\venv
+set VIRTUAL_ENV=C:\Users\Berantemyok\Desktop\UTS\SEMESTER 3\pl sql\UTS PL SQL\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
